7f57130a0cbcf3297c18dead6d524b19ff5fa057
vvitkov <zeridon@users.noreply.github.com>
2018-10-31 16:27:11 +0200

    Enable internal logic for streamcounting

--- monitoring/custom-scripts/check_mk-agent/count-streams.sh
+++ monitoring/custom-scripts/check_mk-agent/count-streams.sh
@@ -8,17 +8,17 @@ if [ -e $MK_CONFDIR/count-streams.cfg ] ; then
     . $MK_CONFDIR/count-streams.cfg
 fi
 
+echo "<<<mrpe>>>"
 if [ ! -r $MK_CONFDIR/count-streams.xsl ] ; then
     _res=3
     _data="UNKNOWN: Stylesheet not found"
 fi
 
-echo "<<<stream_count>>>"
 for STREAMER in ${STREAMERS} ; do
     stream_count=`curl -sfq -m1 ${STREAMER} | xsltproc $MK_CONFDIR/count-streams.xsl -`
 
-    _warn=${1:-3}
-    _crit=${2:-6}
+    _warn=${WARNLEVEL:-3}
+    _crit=${CRITLEVEL:-6}
 
     if [ $stream_count -ge $_crit ] ; then
             _data="OK - stream count $stream_count"
